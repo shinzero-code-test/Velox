ab41c9fd7375352a4d78063fba174f688f79e0fb
20Youssef10 <youssef2010.mahmoud@gmail.com>
2026-08-24 16:25:59 +0000

    Add missing flow.first import

--- player/service/src/main/java/com/exapps/velox/player/service/MediaControllerPlayerController.kt
+++ player/service/src/main/java/com/exapps/velox/player/service/MediaControllerPlayerController.kt
@@ -31,6 +31,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
